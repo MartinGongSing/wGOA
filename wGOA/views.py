e3d6358972eccfae91f21ebeef66fb62412a7f00
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2022-02-15 15:52:18 +0000

    basic navbar (no css)

--- wGOA/views.py
+++ wGOA/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
 
 def index(request):
-    return HttpResponse("Hello world ! ")
+    # return HttpResponse("Hello world ! ")
+    context = {'segment': 'index'}
+
+    html_template = loader.get_template('index.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+def instruments(request):
+    return HttpResponse("this is the instrument page")
